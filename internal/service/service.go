671c66aae4f81170b8182b7b3046b7a5b7ab3252
Vacheslav <slavas2003s@mail.ru>
2026-03-28 17:27:24 +0300

    1try

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Service(input string) (string, error) {
+
+	if strings.TrimSpace(input) == "" {
+		return "", fmt.Errorf("пустая строка")
+	}
+
+	isMorse := true
+
+	runes := []rune(input)
+	for _, r := range runes {
+		if r == '.' || r == '-' || unicode.IsSpace(r) {
+			continue
+		}
+
+		isMorse = false
+		break
+	}
+
+	if isMorse == true {
+		text := morse.ToText(input)
+		return text, nil
+	}
+	textmorse := morse.ToMorse(input)
+	return textmorse, nil
+
+}
